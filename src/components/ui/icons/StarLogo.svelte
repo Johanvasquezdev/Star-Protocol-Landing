31c9fb9424fbd90655a8c7520db07256be22da99
johanvasquezdev <20251235@itla.edu.do>
2026-07-28 11:34:30 -0400

    feat: replace Astro logo with 4-pointed sparkle star logo for branding

---
+++ src/components/ui/icons/StarLogo.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let size = 24;
+  export let className = "";
+  export let style = "";
+</script>
+
+<svg 
+  width={size} 
+  height={size} 
+  viewBox="0 0 24 24" 
+  fill="none" 
+  xmlns="http://www.w3.org/2000/svg"
+  class={className}
+  style={style}
+>
+  <path d="M12 1L14.5 8.5L22 11L14.5 13.5L12 21L9.5 13.5L2 11L9.5 8.5L12 1Z" fill="currentColor"/>
+  <path d="M12 4L13.5 9.5L19 11L13.5 12.5L12 18L10.5 12.5L5 11L10.5 9.5L12 4Z" fill="#ffffff" opacity="0.5"/>
+</svg>
